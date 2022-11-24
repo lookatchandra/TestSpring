fd6045c967a199d9f2f63e8157f465287ae31136
lookatchandra <lookatchandra@gmail.com>
2022-11-24 10:09:46 +0530

    Add file for TestSpring Project

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:8
+EXPOSE 8080
+#ARG JAR_FILE=target/test.jar
+ADD target/spring-test.jar spring-test.jar
+ENTRYPOINT ["java","-jar","/spring-test.jar"]
---
+++ src/main/java/com/test/TestMain.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/test")
+public class TestMain {
+
+	@GetMapping("/message")
+	public String getMain() {
+		return "Hello World!!!!!";
+	}
+	
+	public static void main(String[] args)  throws Exception{
+		SpringApplication.run(TestMain.class, args);
+	}
+}
